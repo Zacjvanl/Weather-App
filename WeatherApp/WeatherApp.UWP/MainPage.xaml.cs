180e7b59065d45a518a2cbab3b57c51cdc56e89b
Micheal Singleton <micwsing@gmail.com>
2017-08-26 15:43:53 -0400

    Created master branch with original project implemented.

---
+++ WeatherApp/WeatherApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherApp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new WeatherApp.App());
+        }
+    }
+}
